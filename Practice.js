2e8c039850ebcc6aab7a82426a554e9b93badac3
Balakrishnan-10 <krishnabala1495@gmail.com>
2024-12-27 11:26:03 +0530

    Generate the Fibonacci sequence up to Nth terms.

--- Practice.js
+++ Practice.js
@@ -3,12 +3,12 @@
 function factorial(n) {
   console.log(n);
   let product = 1;
- //* For Loop :
-    for (let i = n; i > 0; i--) {
-      product = product * i;
-    }
-    return `The factorial of ${n} is ${product}`;
+  //* For Loop :
+  for (let i = n; i > 0; i--) {
+    product = product * i;
   }
+  return `The factorial of ${n} is ${product}`;
+}
 
 //* while loop :
 //   let product1 = 1;
@@ -21,5 +21,25 @@ function factorial(n) {
 
 console.log(factorial(5));
 console.log(factorial(7));
+
+//!------------------------------------------------------
+
+//* Generate the Fibonacci sequence up to Nth terms.
+
+function fibonacciOfNTerm(n) {
+  console.log(n);
+  const fibo = [];
+  if (n >= 1) fibo.push(0);
+  if (n >= 2) fibo.push(1);
+  if (n >= 3) {
+    for (let i = 2; i < n; i++) {
+      fibo.push(fibo[i - 1] + fibo[i - 2]);
+    }
+  }
+  return `The Fibonacci series of ${n} is ${fibo}`;
+}
+console.log(fibonacciOfNTerm(0));
+console.log(fibonacciOfNTerm(1));
+console.log(fibonacciOfNTerm(5));
 
 //!------------------------------------------------------
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 # Maths and Patterns using JavaScript :
 
 1. Calculate the factorial of a number.
+2. Generate the Fibonacci sequence up to Nth terms.
